5ccd140fbdade9b20b20601b44f99957641f60f1
gupta-arpan <arpan6095@gmail.com>
2023-09-17 17:19:18 +0530

    SignUp interface created

--- src/app/components/seller-auth/seller-auth.component.ts
+++ src/app/components/seller-auth/seller-auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SellerService } from 'src/app/services/seller.service';
 import { Router } from '@angular/router';
+import { SignUp } from 'src/app/dataType';
 
 @Component({
   selector: 'app-seller-auth',
@@ -10,7 +11,7 @@ import { Router } from '@angular/router';
 export class SellerAuthComponent {
 
   constructor(private seller:SellerService, private router:Router){}
-  signUp(data:object):void{
+  signUp(data:SignUp):void{
     this.seller.sellerSignUp(data).subscribe((result)=>{
       if(result){
         this.router.navigate(['seller-home']);
--- src/app/services/seller.service.ts
+++ src/app/services/seller.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
+import { SignUp } from '../dataType';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +8,7 @@ import {HttpClient} from '@angular/common/http'
 export class SellerService {
 
   constructor(private http:HttpClient) { }
-  sellerSignUp(data:any){
+  sellerSignUp(data:SignUp){
     return this.http.post('http://localhost:3000/seller',data);
   }
 }
