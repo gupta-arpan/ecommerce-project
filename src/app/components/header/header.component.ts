21b2d26476faf04e48ea3c8e26cce52c9b1a482e
gupta-arpan <arpan6095@gmail.com>
2023-09-19 19:41:34 +0530

    nav menu for seller added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  menuType:string = 'default';
+  constructor(public route:Router){}
+  ngOnInit():void {
+    this.route.events.subscribe((val:any)=>{
+      if(val.url){
+        if(localStorage.getItem('seller') && val.url.includes('seller')){
+          this.menuType = 'seller';
+        }
+        else{
+          this.menuType = 'default';
+        }
+      }
+    })
+  }
 }
