021d1307b46f2fd4844947d4a3cdc3a041b4d041
gupta-arpan <arpan6095@gmail.com>
2023-09-19 21:52:41 +0530

    Seller Name added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,12 +8,20 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent {
   menuType:string = 'default';
+  sellerName:string = '';
   constructor(public route:Router){}
   ngOnInit():void {
     this.route.events.subscribe((val:any)=>{
       if(val.url){
         if(localStorage.getItem('seller') && val.url.includes('seller')){
           this.menuType = 'seller';
+          if(localStorage.getItem('seller')){
+            let sellerStore = localStorage.getItem('seller');
+            let sellerData = sellerStore && JSON.parse(sellerStore)[0];
+            if(sellerData){
+              this.sellerName = sellerData.name;
+            }
+          }
         }
         else{
           this.menuType = 'default';
