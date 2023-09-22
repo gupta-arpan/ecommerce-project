119926b3d668f598456684c1eb470d580554dc0b
gupta-arpan <arpan6095@gmail.com>
2023-09-22 22:19:44 +0530

    prduct fetch api integrated

--- src/app/components/seller-home/seller-home.component.ts
+++ src/app/components/seller-home/seller-home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/dataType';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-seller-home',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./seller-home.component.css']
 })
 export class SellerHomeComponent {
-
+  productList: undefined | Product[];
+  constructor(private productService: ProductService){}
+  ngOnInit(){
+    this.productService.getProducts().subscribe((result)=>{
+      console.log(result);
+      this.productList = result;
+    })
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -12,4 +12,8 @@ export class ProductService {
   addProduct(productData: Product){
    return this.http.post('http://localhost:3000/products', productData);
   }
+
+  getProducts(){
+   return this.http.get<Product[]>('http://localhost:3000/products');
+  }
 }
